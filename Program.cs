b566d1b4a84fde9d013252a4c31c1ab23a8fd9ab
Lucas <43614096+Twistere@users.noreply.github.com>
2020-06-12 14:07:20 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _42_piscine_decount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
