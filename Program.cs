cdef263a67f8ad7d98bc1ec04dcd95f0469c61ac
Lucas <43614096+Twistere@users.noreply.github.com>
2020-06-12 15:26:30 +0200

    Add module

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime;
+using Tweetinvi;
 
 namespace _42_piscine_decount
 {
@@ -6,7 +8,20 @@ namespace _42_piscine_decount
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           var userCredentials = Auth.CreateCredentials("2QAI6K3yau3BWaL5MauQuTiHM", "CmrmPNiHYDkWakRLtH8ob7tqiMmGvKkSGYCRvs9Fze1q1Ge1A0","2601286844-8ibhHLzKrbiFwOqC8tv9ecRmFbnV2NMmjElTNRM", "kyntRBYjBYLsqr92CyWbtDSArsCKdP8m3aWB3yiGdcgU4");
+
+           var authenticatedUser = User.GetAuthenticatedUser();
+
+           Console.WriteLine(authenticatedUser);
+           Console.ReadKey();
+
+           var tweet = Tweet.PublishTweet("C#");
+
+           
+
+        
+
+
         }
     }
 }
