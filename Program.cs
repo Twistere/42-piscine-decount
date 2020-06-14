7134f8e95b591cfdd5b28118a349adeb4f3aac56
Lucas <43614096+Twistere@users.noreply.github.com>
2020-06-14 22:33:26 +0200

    Patch security

--- Program.cs
+++ Program.cs
@@ -8,20 +8,13 @@ namespace _42_piscine_decount
     {
         static void Main(string[] args)
         {
-           var userCredentials = Auth.CreateCredentials("2QAI6K3yau3BWaL5MauQuTiHM", "CmrmPNiHYDkWakRLtH8ob7tqiMmGvKkSGYCRvs9Fze1q1Ge1A0","2601286844-8ibhHLzKrbiFwOqC8tv9ecRmFbnV2NMmjElTNRM", "kyntRBYjBYLsqr92CyWbtDSArsCKdP8m3aWB3yiGdcgU4");
+           var userCredentials = Auth.CreateCredentials("", "","", "");
 
            var authenticatedUser = User.GetAuthenticatedUser();
-
-           Console.WriteLine(authenticatedUser);
-           Console.ReadKey();
 
            var tweet = Tweet.PublishTweet("C#");
 
            
-
-        
-
-
         }
     }
 }
